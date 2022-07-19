81d11ec28213e98e404c7ee0f52110b06aa011cb
YaroslavHorlach <horlachyaroslav@gmail.com>
2022-07-19 19:29:01 +0300

    First java programm after the test one (They are pretty much the same, but it`s an example, so it`s fine)

---
+++ Java/Example_1/Example.java
@@ -0,0 +1,11 @@
+package Java.Example_1;
+
+/*
+ * Пример простой программы на java
+ */
+public class Example {
+    public static void main(String[] args)
+    {
+        System.out.println("Java правит интернетом!");
+    }
+}
