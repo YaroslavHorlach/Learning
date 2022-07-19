d8b0e74611a481910d08f52efb17e4df9764a7a4
YaroslavHorlach <horlachyaroslav@gmail.com>
2022-07-19 19:58:03 +0300

    Variables and their basic usage (assigning and printing them in console)

---
+++ Java/Example_2/Example_2.java
@@ -0,0 +1,20 @@
+package Java.Example_2;
+
+public class Example_2 {
+    //Демонстрация использования переменных
+    public static void main(String[] args) {
+        //объявление переменных
+        int var1;
+        int var2;
+
+        //присваивание var1 значения 1024
+        var1 = 1024;
+
+        System.out.println("Переменная var1 содержит: " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("Переменная var2 содержит var1/2: ");
+        System.out.println(var2);
+    }
+}
