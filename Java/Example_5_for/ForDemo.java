f90e2b31b34bf10de039fa71cbd59690232c4ff4
YaroslavHorlach <horlachyaroslav@gmail.com>
2022-07-20 15:30:33 +0300

    For loop usage

---
+++ Java/Example_5_for/ForDemo.java
@@ -0,0 +1,15 @@
+package Java.Example_5_for;
+
+//Демонстрация применения цикла for
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5; count++) 
+        {
+            System.out.println("Значение счетчика: " + count);
+        }
+
+        System.out.println("Готово!");
+    }
+}
